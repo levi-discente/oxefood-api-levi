fbfb7d7f27e4dd035e042fdc5b12caf02d067cc8
levi-discente <lrsa1@discente.ifpe.edu.br>
2025-04-10 19:21:48 -0300

    feat: entidades

--- src/main/java/br/com/ifpe/oxefood/OxefoodApiLeviApplication.java
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiLeviApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OxefoodApiLeviApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OxefoodApiLeviApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(OxefoodApiLeviApplication.class, args);
+  }
 
 }
